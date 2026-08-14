b4686349498f2341d93b1bd5ddd2bed0379a0e16
samwaes <119363006+samwaes@users.noreply.github.com>
2026-08-14 23:56:16 +0200

    Allow Phase 1 photo and document uploads

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "20mb"
+    }
+  }
 };
 
 export default nextConfig;
